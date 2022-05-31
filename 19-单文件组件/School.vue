e05817e00e80833ec92c355f49a017b85aa4de6d
mr_lululu <1046279147@qq.com>
2022-05-31 15:28:55 +0800

    vue组件化笔记

---
+++ 19-单文件组件/School.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <h2>学校信息</h2>
+      <h3>学校名称：{{name}}</h3>
+      <h3>学校地址：{{address}}</h3>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'School',
+        data() {
+            return {
+                name: '尚硅谷',
+                address: '重庆'
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ 19-单文件组件/Student.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <h2>学生信息</h2>
+      <h3>学生姓名：{{name}}</h3>
+      <h3>学生年龄：{{age}}</h3>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'Student',
+        data() {
+            return {
+                name: '鹿先森',
+                age: 18
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
